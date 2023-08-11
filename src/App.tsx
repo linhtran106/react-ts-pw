1ecf48633e0e4fbe63e599213a0b57825075cb5a
Linh Tran <linhtran.4work@gmail.com>
2023-08-12 00:17:34 +0700

    test

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p data-cy="description">
-          Edit <code>src/App.tsx</code> and save to reload. Test test 123
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
